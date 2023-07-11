6fb00af8da211117af1f351418559a541b2deb00
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2023-07-11 00:26:22 -0700

    Update 𝘼𝙑𝙄-gdrive.js

--- plugins/𝘼𝙑𝙄-gdrive.js
+++ plugins/𝘼𝙑𝙄-gdrive.js
@@ -2,27 +2,27 @@
 import fg from 'api-dylux' 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 
-	if (!args[0]) throw `💖Prince avi md 💖, Enter a Google Drive link`
+	if (!args[0]) throw `✳️Google Drive වෙබ් ලිපිනය දෙන්න`
 	m.react(rwait) 
 	try {
 	let res = await fg.GDriveDl(args[0])
 	 await m.reply(`
-*💖Prince avi md 💖 Google Drive DOWNLOADER*
+≡ *Google Drive DL*
 
-💝 *Number:* ${res.fileName}
-💝 *Size:* ${res.fileSize}
-💝 *type:* ${res.mimetype}`)
+▢ *Number:* ${res.fileName}
+▢ *Size:* ${res.fileSize}
+▢ *type:* ${res.mimetype}`)
 		
 	conn.sendMessage(m.chat, { document: { url: res.downloadUrl }, fileName: res.fileName, mimetype: res.mimetype }, { quoted: m })
 	m.react(done)
    } catch {
-	m.reply('💝 Error: Check the link or try another link එක බෑ වෙන ලින්ක් එකක් බලහන් 💝') 
+	m.reply('Error: Check the link or try another link') 
   }
 }
 handler.help = ['gdrive']
 handler.tags = ['dl', 'prem']
 handler.command = ['gdrive']
-handler.diamond = false
-handler.premium = false
+handler.diamond = true
+handler.premium = true
 
 export default handler
