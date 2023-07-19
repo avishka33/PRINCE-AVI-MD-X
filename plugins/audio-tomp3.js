b18b91a1fb236e730bd24c5c72560e5c7697ef09
AVISHKA SHAVINDA <134716496+avishka33@users.noreply.github.com>
2023-07-19 20:18:10 +0530

    Update audio-tomp3.js
    🗡️AVI🗡️

--- plugins/audio-tomp3.js
+++ plugins/audio-tomp3.js
@@ -3,11 +3,11 @@ import { toAudio } from '../lib/converter.js'
 let handler = async (m, { conn, usedPrefix, command }) => {
     let q = m.quoted ? m.quoted : m
    /* let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-    if (!/video|audio/.test(mime)) throw ` Mention to 💖Prince avi md 💖 a video or document for convert to mp3,with the command :\n\n*${usedPrefix + command}*`*/
+    if (!/video|audio/.test(mime)) throw ` Mention to 🗡️PRINCE AVI MD 🗡️ a video or document for convert to mp3,with the command :\n\n*${usedPrefix + command}*`*/
     let media = await q.download?.()
-    if (!media) throw 'Failed on 💖Prince avi md 💖 Server'
+    if (!media) throw 'Failed on 🗡️PRINCE AVI MD🗡️ Server'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw ' Error 💖Prince avi md 💖 Server'
+    if (!audio.data) throw ' Error 🗡️PRINCE AVI MD🗡️ Server'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
 }
 handler.help = ['tomp3']
